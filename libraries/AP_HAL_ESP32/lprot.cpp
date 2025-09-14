b315e2f63498e674e67e85304e0d98fd05b62b44
bigl <42046805+lucodest@users.noreply.github.com>
2025-09-14 17:04:26 -0300

    implement my own serial protocol definition

---
+++ libraries/AP_HAL_ESP32/lprot.cpp
@@ -0,0 +1,139 @@
+#include "lprot.h"
+#include "LSPDriver.h"
+#include "AP_Math/crc.h"
+
+using namespace ESP32;
+
+LProt* LProt::singleton_ = nullptr;
+
+LProt* LProt::instance() {
+    if(singleton_ == nullptr) {
+        singleton_ = new LProt();
+    }
+
+    return singleton_;
+}
+
+LProt::LProt() {
+    pkt_buffer = new uint8_t[261];
+}
+
+inline void LProt::handleSensorData(uint8_t type, uint8_t* data, uint8_t len) {
+    switch (type)
+    {
+        case BARO:
+            handleBaroData(*((BARO_DATA_t*) data));
+            break;
+        case MAG:
+            handleMagData(*((MAG_DATA_t*) data));
+            break;
+        case IMU:
+            handleImuData(*((IMU_DATA_t*) data));
+        default:
+
+            break;
+    }
+}
+
+void LProt::handlePayload(uint8_t type, uint8_t* data, uint8_t len){
+    switch (type >> 4)
+    {
+        case STREAM:
+            handleMultiStream(type & 0xF, data, len);
+            break;
+        case SENSOR:
+            handleSensorData(type & 0xF, data, len);
+            break;
+        default:
+
+            break;
+    }
+}
+
+uint16_t LProt::calculateCrc(uint8_t* data, uint16_t len) {
+    uint32_t crc = crc_crc32(0xFFFFFFFF, data, len);
+    uint8_t rem = len % 4;
+
+    if(rem){
+        uint8_t zr[] = {0x0, 0x0, 0x0};
+        crc = crc_crc32(crc, zr, 4 - rem);
+    }
+
+    return crc;
+}
+
+void LProt::handleRx(uint8_t* data, uint16_t len){
+
+    for(uint16_t i = 0; i < len; i++){
+        pkt_buffer[pkt_index] = data[i];
+        pkt_index++;
+
+        //Start of frame recived
+        if(inPacket){
+
+            //Got lenght
+            if(pkt_index > 2){
+                uint8_t pkt_size = 5 + pkt_buffer[2];
+                //Last byte of packet
+                if(pkt_size == pkt_index){
+                    //Debug
+                    //handleError(2, pkt_buffer, pkt_size);
+
+                    pkt_size -= 2;
+                    uint16_t calc_crc = calculateCrc(pkt_buffer, pkt_size);
+                    uint16_t pkt_crc = (pkt_buffer[pkt_size] << 8) | pkt_buffer[pkt_size+1];
+
+                    //Packet OK
+                    if(pkt_crc == calc_crc){
+                        handlePayload(pkt_buffer[1], pkt_buffer + 3, pkt_buffer[2]);
+                    //Crc error
+                    }else{
+                        handleError(1, calc_crc, pkt_crc);
+                        crc_errors++;
+                    }
+
+                    pkt_index = 0;
+                    inPacket = false;
+                }
+            }
+
+        }else if(data[i] == LPROT_MAGIC){
+            inPacket = true;
+        }else{
+            //Discard byte
+            pkt_index--;
+        }
+    }
+}
+
+void LProt::sendFrame(uint8_t type, uint8_t* payload, uint8_t len) {
+    uint8_t buffer[261];
+    //Assemble frame
+    buffer[0] = LPROT_MAGIC;
+    buffer[1] = type;
+    buffer[2] = len;
+    //Copy payload
+    uint16_t i = 0;
+    for(; i < len; i++){
+        buffer[3 + i] = payload[i];
+    }
+    //Total size
+    i = 3 + len;
+    //Calculate crc
+    uint16_t crc = calculateCrc(buffer, i);
+    buffer[i] = crc >> 8;
+    buffer[i+1] = crc & 0xFF;
+
+    handleTx(buffer, i + 2);
+}
+
+void LProt::sendMultiStream(uint8_t id, uint8_t* data, uint8_t len) {
+    sendFrame((STREAM << 4) | (id & 0xf), data, len);
+}
+
+void LProt::handleMultiStream(uint8_t id, uint8_t* data, uint8_t len) {
+    LSPDriver* vu = LSPDriver::get(id);
+    if(vu != NULL) {
+        vu->back_write(data, len);
+    }
+}
---
+++ libraries/AP_HAL_ESP32/lprot.h
@@ -0,0 +1,115 @@
+#pragma once
+
+#include "AP_HAL_ESP32.h"
+
+//My protocol
+
+/* 
+Packet format
+
+byte 0 - Magic 0xE9
+byte 1 - Packet type
+byte 2 - Payload length n
+bytes 3 - n Payload
+bytes n+1 / n+2 - CRC32 only first 2 bytes
+
+Packet types
+Most significant 4 bits
+
+0x2 - Multiplexed Stream data
+    Next 4 bits are the stream id 0-15
+    Payload is data to be written to the stream
+
+0x3 - Sensor data
+    Next 4 bits are the sensor type (see enum)
+    Payload is sensor data structure
+*/
+
+namespace ESP32 
+{
+
+#define LPROT_MAGIC 0xE9
+
+class LProt 
+{
+public:
+
+    LProt(LProt &other) = delete;
+
+    void operator=(const LProt &) = delete;
+
+    static LProt* instance();
+
+    typedef enum {
+        STREAM = 2,
+        SENSOR = 3
+    } LPROT_PACKET_TYPE;
+
+    typedef enum {
+        //Barometer
+        BARO = 1,
+        //3-Axis Magnetometer (Compass)
+        MAG = 2,
+
+        IMU = 3
+    } LPROT_SENSOR_TYPE;
+
+    typedef struct PACKED {
+        uint32_t pressure;  //In Pascal (Abs)
+        uint16_t temperature;   //Temperature in 0.1 Celcius
+    } BARO_DATA_t;
+
+    typedef struct PACKED {
+        //In miliGauss
+        int16_t mag_x;
+        int16_t mag_y;
+        int16_t mag_z;
+    } MAG_DATA_t;
+
+    typedef struct PACKED {
+        int16_t ax;
+        int16_t ay;
+        int16_t az;
+        int16_t gx;
+        int16_t gy;
+        int16_t gz;
+    } IMU_DATA_t;
+
+    //Handle raw recived data, MUST be called by user
+    void handleRx(uint8_t* data, uint16_t len);
+
+    //Functions for sending frames, all are called by user
+    void sendMultiStream(uint8_t id, uint8_t* data, uint8_t len);
+
+protected:
+
+    LProt();
+
+    static LProt* singleton_;
+
+private:
+    uint32_t crc_errors = 0;
+    bool inPacket = false;
+    uint16_t pkt_index = 0;
+    uint8_t *pkt_buffer;
+
+    inline void handleSensorData(uint8_t type, uint8_t* data, uint8_t len);
+    void handlePayload(uint8_t type, uint8_t* data, uint8_t len);
+    void sendFrame(uint8_t type, uint8_t* payload, uint8_t len);
+
+    //Function for calculating crc, MUST be implemented by user
+    uint16_t calculateCrc(uint8_t* data, uint16_t len);
+
+    //Functions callbacks for handling frame types, all implemented by user
+    void handleMultiStream(uint8_t id, uint8_t* data, uint8_t len);
+    void handleBaroData(BARO_DATA_t data);
+    void handleMagData(MAG_DATA_t data);
+    void handleImuData(IMU_DATA_t data);
+
+    //Handle raw data to transmitt, MUST be implemented by user
+    void handleTx(uint8_t* data, uint16_t len);
+
+    void handleError(uint8_t err, ...);
+};
+
+}
