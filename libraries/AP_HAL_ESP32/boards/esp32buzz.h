8012ade5c533ca640205de5111a09716ae826faf
bigl <42046805+lucodest@users.noreply.github.com>
2025-09-14 23:40:25 -0300

    import old board configuration

--- libraries/AP_HAL_ESP32/boards/esp32buzz.h
+++ libraries/AP_HAL_ESP32/boards/esp32buzz.h
@@ -30,12 +30,15 @@
 #define PROBE_MAG_IMU_I2C(driver, imudev, bus, addr, args ...) ADD_BACKEND(DRIVER_ ##driver, AP_Compass_ ## driver::probe_ ## imudev(GET_I2C_DEVICE(bus,addr),##args))
 //------------------------------------
 
+//My Custom multiplexing protocol
+#define DEFAULT_SERIAL2_PROTOCOL SerialProtocol_None
+#define DEFAULT_SERIAL2_BAUD 115200
 
 //#define CONFIG_HAL_BOARD 12
 //#define HAL_BOARD_ESP32 12
 
 //INS choices:
-#define HAL_INS_DEFAULT HAL_INS_MPU9250_SPI
+#define HAL_INS_DEFAULT HAL_INS_BMI160_SPI
 //#define HAL_INS_MPU9250_NAME "MPU9250"
 
 // or this:
@@ -51,17 +54,24 @@
 //#define HAL_COMPASS_MAX_SENSORS 3
 
 // IMU probing:
-//#define HAL_INS_PROBE_LIST PROBE_IMU_I2C(Invensensev2, 0, 0x68, ROTATION_YAW_270)
+#define HAL_INS_PROBE_LIST PROBE_IMU_SPI(BMI160, "bmi160", ROTATION_ROLL_180_YAW_90)
 // MAG/COMPASS probing:
 //#define HAL_MAG_PROBE_LIST ADD_BACKEND(DRIVER_ICM20948, AP_Compass_AK09916::probe_ICM20948_I2C(0, ROTATION_NONE));
 // BARO probing:
 //#define HAL_BARO_PROBE_LIST PROBE_BARO_I2C(BMP280, 0, 0x77)
 
 // no airspeed sensor
-#define AP_AIRSPEED_MS4525_ENABLED 0
 #define AP_AIRSPEED_ENABLED 0
 #define AP_AIRSPEED_ANALOG_ENABLED 0
 #define AP_AIRSPEED_BACKEND_DEFAULT_ENABLED 0
+
+// allow boot without a baro
+#define HAL_BARO_ALLOW_INIT_NO_BARO 1
+
+// disable all baros (except one of ur choice)
+#define AP_BARO_PROBE_EXTERNAL_I2C_BUSES 0
+#define AP_BARO_BACKEND_DEFAULT_ENABLED 0
+//#define AP_BARO_BMP085_ENABLED 1
 
 // ADC is available on lots of pints on the esp32, but adc2 can't co-exist with wifi we choose to allow ADC on :
 //#define HAL_DISABLE_ADC_DRIVER 1
@@ -71,46 +81,35 @@
 // the pin number, the gain/multiplier associated with it, the ardupilot name for the pin in parameter/s.
 //
 // two different pin numbering schemes, both are ok, but only one at a time:
-#define HAL_ESP32_ADC_PINS_OPTION1 {\
-	{ADC1_GPIO35_CHANNEL, 11, 1},\
-	{ADC1_GPIO34_CHANNEL, 11, 2},\
-	{ADC1_GPIO39_CHANNEL, 11, 3},\
-	{ADC1_GPIO36_CHANNEL, 11, 4}\
-}
-#define HAL_ESP32_ADC_PINS_OPTION2 {\
-	{ADC1_GPIO35_CHANNEL, 11, 35},\
-	{ADC1_GPIO34_CHANNEL, 11, 34},\
-	{ADC1_GPIO39_CHANNEL, 11, 39},\
-	{ADC1_GPIO36_CHANNEL, 11, 36}\
-}
 // pick one:
-//#define HAL_ESP32_ADC_PINS HAL_ESP32_ADC_PINS_OPTION1
-#define HAL_ESP32_ADC_PINS HAL_ESP32_ADC_PINS_OPTION2
+#define HAL_ESP32_ADC_PINS {{ADC2_GPIO15_CHANNEL, 11, 1}}
+
+#define AP_COMPASS_BACKEND_DEFAULT_ENABLED 0
+//#define AP_COMPASS_QMC5883L_ENABLED 1
+
+//#define HAL_PROBE_EXTERNAL_I2C_COMPASSES 1
 
 
-
-#define HAL_PROBE_EXTERNAL_I2C_COMPASSES 1
-
-
-#define HAL_INS_MPU9250_NAME "mpu9250"
+//#define HAL_INS_MPU9250_NAME "mpu9250"
 
 // uncommenting one or more of these will give more console debug in certain areas.. ... 
 // ...however all teh extra printf's use a lot of stack, so best to limit yourself to only uncommenting one at a time
 //#define STORAGEDEBUG 1
-//#define SCHEDDEBUG 1
+#define SCHEDDEBUG 1
 //#define FSDEBUG 1
 //#define BUSDEBUG 1 //ok
 //#define WIFIDEBUG 1 //uses a lot?
 //#define INS_TIMING_DEBUG 1 //define this to see all the imu-resets and temp resets and imu timing info on the console.
 
-#define HAL_INS_PROBE_LIST PROBE_IMU_SPI( Invensense, HAL_INS_MPU9250_NAME, ROTATION_NONE)
+//#define HAL_INS_PROBE_LIST PROBE_IMU_SPI( Invensense, HAL_INS_MPU9250_NAME, ROTATION_NONE)
 //#define HAL_INS_PROBE_LIST PROBE_IMU_SPI( Invensense, HAL_INS_MPU9250_NAME, ROTATION_ROLL_180)
 
+//#define HAL_BARO_PROBE_LIST PROBE_BARO_I2C(BMP085, 0, 0xEF)
 
-#define HAL_BARO_PROBE_LIST PROBE_BARO_SPI(BMP280, "bmp280")
+//#define HAL_MAG_PROBE_LIST PROBE_MAG_I2C(QMC5883L, 0, 0x0D, false, ROTATION_NONE)
 
 // 2 use udp, 1 use tcp...  for udp,client needs to connect as UDPCL in missionplanner etc to 192.168.4.1 port 14550
-#define HAL_ESP32_WIFI 1
+//#define HAL_ESP32_WIFI 1
 
 // tip: if u are ok getting mavlink-over-tcp or mavlink-over-udp and want to disable mavlink-over-serial-usb
 //then set ardupilot parameter SERIAL0_PROTOCOL = 0 and reboot.
@@ -119,6 +118,11 @@
 //LOG_DISARMED 1
 //SERIAL0_PROTOCOL 0
 
+
+// see boards.py
+#ifndef ENABLE_HEAP
+#define ENABLE_HEAP 1
+#endif
 
 #define WIFI_SSID "ardupilot123"
 #define WIFI_PWD "ardupilot123"
@@ -129,44 +133,42 @@
 
 // SPI BUS setup, including gpio, dma, etc
 // note... we use 'vspi' for the bmp280 and mpu9250
-#define HAL_ESP32_SPI_BUSES \
-    {.host=VSPI_HOST, .dma_ch=1, .mosi=GPIO_NUM_23, .miso=GPIO_NUM_19, .sclk=GPIO_NUM_18}
 // tip:  VSPI_HOST  is an alternative name for esp's SPI3
-//#define HAL_ESP32_SPI_BUSES {}
+#define HAL_ESP32_SPI_BUSES \
+    {.host=SPI2_HOST, .dma_ch=SPI_DMA_CH_AUTO, .mosi=GPIO_NUM_13, .miso=GPIO_NUM_14, .sclk=GPIO_NUM_15}
 
 // SPI per-device setup, including speeds, etc.
 #define HAL_ESP32_SPI_DEVICES \
-    {.name= "bmp280", .bus=0, .device=0, .cs=GPIO_NUM_26, .mode = 3, .lspeed=1*MHZ, .hspeed=1*MHZ}, \
-    {.name="mpu9250", .bus=0, .device=1, .cs=GPIO_NUM_5,  .mode = 0, .lspeed=2*MHZ, .hspeed=8*MHZ}
-//#define HAL_ESP32_SPI_DEVICES {}
+    {.name= "bmi160", .bus=0, .device=1, .cs=GPIO_NUM_16, .mode = 3, .lspeed=1*MHZ, .hspeed=8*MHZ}
 
 //I2C bus list
 #define HAL_ESP32_I2C_BUSES \
-	{.port=I2C_NUM_0, .sda=GPIO_NUM_13, .scl=GPIO_NUM_12, .speed=400*KHZ, .internal=true}
+	{.port=I2C_NUM_0, .sda=GPIO_NUM_23, .scl=GPIO_NUM_26, .speed=400*KHZ, .internal=true}
 //#define HAL_ESP32_I2C_BUSES {} // using this embty block appears to cause crashes?
 
 
 // rcin on what pin?
-#define HAL_ESP32_RCIN GPIO_NUM_4
-
+#define HAL_ESP32_RCIN GPIO_NUM_12
+#define HAL_ESP32_RMT_RX_PIN_NUMBER 12
 
 //HARDWARE UARTS
 #define HAL_ESP32_UART_DEVICES \
-  {.port=UART_NUM_0, .rx=GPIO_NUM_3, .tx=GPIO_NUM_1 },{.port=UART_NUM_1, .rx=GPIO_NUM_16, .tx=GPIO_NUM_17 }
+  {.port=UART_NUM_0, .rx=GPIO_NUM_3, .tx=GPIO_NUM_1 }, {.port=UART_NUM_1, .rx=GPIO_NUM_4, .tx=GPIO_NUM_2 }
 
-#define AP_FILESYSTEM_ESP32_ENABLED 1
+// #define AP_FILESYSTEM_ESP32_ENABLED 1
 
 // Do u want to use mmc or spi mode for the sd card, this is board specific ,
 //  as mmc uses specific pins but is quicker,
-#define HAL_ESP32_SDMMC 1
+// #define HAL_ESP32_SDMMC 1
 // and spi is more flexible pinouts....  dont forget vspi/hspi should be selected to NOT conflict with SPI_BUSES above
 //#define HAL_ESP32_SDSPI {.host=VSPI_HOST, .dma_ch=2, .mosi=GPIO_NUM_2, .miso=GPIO_NUM_15, .sclk=GPIO_NUM_14, .cs=GPIO_NUM_21}
 
-#define HAL_ESP32_SDCARD 1
-#define LOGGER_MAVLINK_SUPPORT 1
+// #define HAL_ESP32_SDCARD 1
+// #define LOGGER_MAVLINK_SUPPORT 1
+#define HAL_BOARD_LOG_DIRECTORY "/SDCARD/APM/LOGS"
+#define HAL_BOARD_TERRAIN_DIRECTORY "/SDCARD/APM/TERRAIN"
+#define HAL_BOARD_STORAGE_DIRECTORY "/SDCARD/APM/STORAGE"
 
 // this becomes the default value for the ardupilot param LOG_BACKEND_TYPE, which most ppl want to be 1, for log-to-flash
 // setting to 2 means log-over-mavlink to a companion computer etc.
-#define HAL_LOGGING_BACKENDS_DEFAULT 1
-
-#define HAL_ESP32_RMT_RX_PIN_NUMBER 4
+#define HAL_LOGGING_BACKENDS_DEFAULT 2
