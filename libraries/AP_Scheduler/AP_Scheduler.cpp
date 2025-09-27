c6f2f622a95066434c593f2a797bf8b3e0bb398b
bigl <42046805+lucodest@users.noreply.github.com>
2025-09-26 23:17:48 -0300

    debug fast tasks timing

--- libraries/AP_Scheduler/AP_Scheduler.cpp
+++ libraries/AP_Scheduler/AP_Scheduler.cpp
@@ -276,6 +276,13 @@ void IRAM_ATTR AP_Scheduler::run(uint32_t time_available)
         // work out how long the event actually took
         now = AP_HAL::micros();
         uint32_t time_taken = now - _task_time_started;
+
+        //Debug
+        if (task.priority <= MAX_FAST_TASK_PRIORITIES && _tick_counter % 5 == 0) {
+            //Fast task
+            hal.console->printf("%s %luus\n", task.name, time_taken);
+        }
+
         bool overrun = false;
         if (time_taken > _task_time_allowed) {
             overrun = true;
